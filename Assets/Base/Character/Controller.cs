21deb475084afc8c5b7a45c0cfbd0d16a7f6a12d
DESKTOP-HG2535O\dlclg <dlclgus2@naver.com>
2021-09-02 04:33:51 +0900

    Character Controller 추가 - 기존 CharacterController 컴포넌트를 활용하는 컴포넌트를 추가하였습니다. - 기본 방향키, 점프, 중력, 가속, 부드러운 Forwarding 등의 기능을 지니고 있습니다. - 시점은 Top view 입니다.
    To do..
    - Movement 내부 시스템 픽스
    - 플레이어 총알 발사 시스템
    - 기타 이전 커밋에서 제시한 이슈들

---
+++ Assets/Base/Character/Controller.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [GetComponent] public CharacterController cController;
+
+    public bool isGround;
+    public float groundCheckerY;
+    public LayerMask groundMask;
+
+    public Vector3 velocity;
+    public float velocityX;
+    public float velocityY;
+    public float velocityZ;
+    public Vector3 forward;
+    public bool useGravity;
+    public float extraGravityY;
+
+    private void Awake()
+    {
+        GetComponentAttributeSetter.DoUpdate_GetComponentAttribute(this);
+    }
+
+    public bool IsGround()
+    {
+        if (cController.isGrounded)
+        {
+            isGround = true;
+            return isGround;
+        }
+
+        Vector3 groundCheckPoint = transform.position + new Vector3(0f, groundCheckerY, 0f);
+        Collider[] others = Physics.OverlapSphere(groundCheckPoint, cController.radius, groundMask, QueryTriggerInteraction.Ignore);
+
+        isGround = (others.Length > 0);
+        return isGround;
+    }
+
+    public void Move(Vector3 _direction, float _speed, float _acceleration)
+    {
+        _direction.Normalize();
+
+        Vector3 desiredVelocity = _direction * _speed;
+
+        Vector3 _velocity = Vector3.Lerp(velocity, desiredVelocity, Time.deltaTime / _acceleration);
+
+        velocityX = Mathf.Lerp(velocity.x, desiredVelocity.x, Time.deltaTime / _acceleration);
+        velocityZ = Mathf.Lerp(velocity.z, desiredVelocity.z, Time.deltaTime / _acceleration);
+        velocityY = Gravity();
+
+        velocity = new Vector3(velocityX, velocityY, velocityZ);
+
+
+        cController.Move(velocity * Time.deltaTime);
+    }
+
+    public float Gravity()
+    {
+        float _velocityY = velocityY;
+        if (useGravity)
+        {
+            if (!isGround)
+            {
+                float gravity = Physics.gravity.y + extraGravityY;
+                gravity = Mathf.Clamp(gravity, -float.MaxValue, -0.001f);
+                _velocityY += gravity * Time.deltaTime;
+            }
+            else
+            {
+                if (_velocityY < 0f)
+                    _velocityY = 0f;
+            }
+        }
+
+        return _velocityY;
+    }
+
+    public void Forwarding(Vector3 _direction, float _acceleration)
+    {
+        _direction.y = 0f;
+        _direction.Normalize();
+
+        forward = Vector3.Slerp(forward, _direction, Time.deltaTime / _acceleration);
+        forward.Normalize();
+
+        if (forward.magnitude > 0f)
+            transform.forward = forward;
+    }
+
+    public void Jump(float _height)
+    {
+        if (isGround)
+        {
+            velocityY = 0f;
+            velocityY += Mathf.Sqrt(2f * Mathf.Abs(Physics.gravity.y + extraGravityY) * _height);
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 groundCheck = transform.position + new Vector3(0, groundCheckerY, 0);
+        Gizmos.DrawWireSphere(groundCheck, cController.radius);
+    }
+#endif
+
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,5 +6,36 @@ using System.Reflection;
 
 public class Player : Character
 {
+    [GetComponent] Controller controller;
+
+    public Vector3 inputDirection = new Vector3();
+
+    [Min(0f)] public float speedAirMultiple;
+    [Min(0f)] public float speed;
+    [Min(0f)] public float accelerationGround;
+    [Min(0f)] public float accelerationAir;
+
+    private void Awake()
+    {
+        GetComponentAttributeSetter.DoUpdate_GetComponentAttribute(this);
+    }
+
+
+    private void Update()
+    {
+        controller.IsGround();
+        float _acceleration = controller.isGround ? accelerationGround : accelerationAir;
+        float _speed = controller.isGround ? speed : speed * speedAirMultiple;
+
+        inputDirection.x = Input.GetAxisRaw("Horizontal");
+        inputDirection.z = Input.GetAxisRaw("Vertical");
+        inputDirection.Normalize();
+
+        controller.Move(inputDirection, _speed, _acceleration);
+        controller.Forwarding(inputDirection, _acceleration);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            controller.Jump(3f);
+    }
 
 }
