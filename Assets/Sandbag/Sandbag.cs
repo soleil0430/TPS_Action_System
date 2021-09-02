12cfc92d86941d06790fa588a8ce234ed706ef4e
DESKTOP-HG2535O\dlclg <dlclgus2@naver.com>
2021-09-03 00:24:50 +0900

    임시 테스트용 빌드파일 생성

--- Assets/Sandbag/Sandbag.cs
+++ Assets/Sandbag/Sandbag.cs
@@ -15,11 +15,29 @@ public class Sandbag : Character
 
     Color color = Color.white;
 
+
+    public Vector3 startPos;
+    [Min(1f)] public float respawnInterval;
+
     private void Awake()
     {
         GetComponentAttributeSetter.DoUpdate_GetComponentAttribute(this);
+        startPos = transform.position;
+
+        StartCoroutine(RespawnRoutine());
     }
 
+    IEnumerator RespawnRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnInterval);
+            transform.position = startPos;
+            nowHp = maxHp;
+            color = new Color(1f, nowHp / maxHp, nowHp / maxHp);
+            render.material.color = color;
+        }
+    }
 
 
 
